2738abaf0fd841ebcc2274bc414098da859a94f3
Yurii Khomiak <luna.loooony.lovegood@gmail.com>
2018-12-12 21:13:18 +0200

    Added more conversion functions

---
+++ convertor.py
@@ -0,0 +1,24 @@
+def km_per_h_to_m_per_sec(speed) :
+    """Converts km/h to m/s"""
+    return speed * (1000 / 3600)
+
+def m_per_sec_to_km_per_h(speed) :
+    """Converts m/s to km/h"""
+    return speed * 3.6
+
+def km_to_m(distance) :
+    """Converts km to m"""
+    return distance * 1000
+
+def m_to_km(distance) :
+    """Converts m to km"""
+    return distance / 1000
+
+def h_to_s(time) :
+    """Converts h to s"""
+    return time / 3600
+
+def s_to_h(time) :
+    """Converts s to h"""
+    return time * 3600
+
