70b9cf0d28c646b8fc13f9117c1bd55c9226e51f
Yurii Khomiak <luna.loooony.lovegood@gmail.com>
2018-12-17 20:21:40 +0200

    Fixed generation of vehicle movement

--- generator.py
+++ generator.py
@@ -10,29 +10,30 @@ def generate_distances(
     number_of_vehicles, number_of_lanes, vehicle_length, 
     road_interval, time_stamps, 
     mean_speed, speed_deviation,
-    spacing, spacing_scatter) :
+    average_number_of_vehicles) :
     
     number_of_time_stamps = time_stamps.size
     time_step = time_stamps[1] - time_stamps[0]
     mean_speed *= time_step
     speed_deviation *= time_step
+    shift = 2.0
 
     distances = np.zeros((number_of_vehicles, number_of_time_stamps))
-    positions = generate_starting_positions(
-        number_of_vehicles, road_interval, number_of_lanes,spacing, spacing_scatter)
+    positions = generate_starting_positions(number_of_vehicles, average_number_of_vehicles, 
+        vehicle_length, road_interval, number_of_lanes)
 
     first_unexistent_vehicle_index = find_first_unexistent_vehicle(positions)
 
     for i in range(0, number_of_time_stamps) :
-        for _ in range(0, number_of_lanes) :
-            if first_unexistent_vehicle_index < number_of_vehicles :
-                first_unexistent_vehicle_index += 1
-            else :
-                break
+        if first_unexistent_vehicle_index < number_of_vehicles :
+            first_unexistent_vehicle_index += int(np.random.uniform(0, number_of_lanes + 1))
+            if first_unexistent_vehicle_index > number_of_vehicles :
+                first_unexistent_vehicle_index = number_of_vehicles
         for j in range(0, first_unexistent_vehicle_index) :
             if positions[j] == road_interval :
                 continue
-            distances[j][i] = np.random.normal(mean_speed, speed_deviation)
+            gamma_shift = np.random.gamma(9, 0.5) - 2.0
+            distances[j][i] = np.random.normal(mean_speed, speed_deviation) + gamma_shift - shift
             if distances[j][i] < 0 :
                 distances[j][i] = 0
             if (positions[j] + distances[j][i]) <= road_interval :
@@ -43,16 +44,22 @@ def generate_distances(
 
     return [distances, positions]
 
-def generate_starting_positions(number_of_vehicles, road_interval, number_of_lanes, 
-                                spacing, spacing_scatter) :
+def generate_starting_positions(number_of_vehicles, average_number_of_vehicles,
+        vehicle_length, road_interval, number_of_lanes) :
     starting_positions = np.zeros(number_of_vehicles)
+    mean_spacing = ((road_interval*number_of_lanes) - (vehicle_length*average_number_of_vehicles)) / average_number_of_vehicles
 
     for i in range(0, number_of_vehicles, number_of_lanes) :
         for j in range(0, number_of_lanes) :
-            position = (spacing * i) + (np.random.random() * spacing_scatter)
             if (i + j) >= number_of_vehicles :
                 break
-            starting_positions[i + j] = position if (position <= road_interval) else 0.0
+            position_shift = np.random.normal(mean_spacing, mean_spacing*0.2)
+            position_shift = position_shift if (position_shift >= 0) else 0
+            if (i - number_of_lanes) >= 0 :
+                position = starting_positions[i - number_of_lanes] + position_shift
+            else :
+                position = position_shift
+            starting_positions[i + j] = position if (position <= road_interval) else 0
 
     first_unexistent_vehicle_index = find_first_unexistent_vehicle(starting_positions)
     reverse_index = first_unexistent_vehicle_index - 1
